e038ed45709036428ad1df7d5bb1e6716cb081b4
EllyIMA <wiz33@yandex.ru>
2022-03-21 14:53:52 +0300

    beta_2

--- bullet.py
+++ bullet.py
@@ -6,9 +6,9 @@ class Bullet(pygame.sprite.Sprite):
         '''СОздаем пулю в позиции пушки'''
         super(Bullet, self).__init__()
         self.screen = screen
-        self.rect = pygame.Rect(0, 0, 2, 12)
+        self.rect = pygame.Rect(0, 0, 5, 12)
         self.color = 0, 187, 212
-        self.speed = 1.5
+        self.speed = 5
         self.rect.centerx = gun.rect.centerx
         self.rect.top = gun.rect.top
         self.y = float(self.rect.y)
--- sace_game.py
+++ sace_game.py
@@ -19,7 +19,7 @@ def run():
         Controls.events(screen,gun,bullets)
         gun.update_gun()
         Controls.update(bg_color, screen, gun, inos, bullets)
-        Controls.update_bullets(inos,bullets)
+        Controls.update_bullets(screen, inos,bullets)
         Controls.update_inos(stats,screen,gun, inos, bullets)
 
 
